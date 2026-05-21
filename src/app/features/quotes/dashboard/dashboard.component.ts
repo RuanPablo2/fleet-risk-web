9d72c6f0220e7028584253f1ba66c85234d8428a
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-05-21 18:49:18 -0300

    dashboard kpi cards

--- src/app/features/quotes/dashboard/dashboard.component.ts
+++ src/app/features/quotes/dashboard/dashboard.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-
+  kpis = {
+    pending: 12,
+    calculated: 5,
+    approved: 28,
+  };
 }
