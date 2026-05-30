9dad362847c81f04ba684d1a4a9d01277e29fbb2
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2026-05-29 23:18:06 -0300

    adjusting responsiveness

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -6,6 +6,10 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../services/auth.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-layout',
@@ -19,12 +23,21 @@ import { AuthService } from '../services/auth.service';
     MatListModule,
     MatIconModule,
     MatButtonModule,
+    AsyncPipe,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
   private authService = inject(AuthService);
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay(),
+    );
 
   logout() {
     this.authService.logout();
